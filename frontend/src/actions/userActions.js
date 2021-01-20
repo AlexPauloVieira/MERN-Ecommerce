3db1f7c19a2a265888ce5ed3c898c1b60cbfe745
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2021-01-19 21:20:47 -0300

    User Edit Screen and Get User Details

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -225,9 +225,6 @@ export const deleteUser = (id) => async (dispatch, getState) => {
       },
     };
 
-    // eslint-disable-next-line
-    const { data } = await axios.delete(`/api/users/${id}`, config);
-
     dispatch({
       type: USER_DELETE_SUCCESS,
     });
